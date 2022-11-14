a184cb9c2e651623ac1c56489c63084e8a723a80
Spokhim <spokhim@hotmail.com>
2022-11-14 11:46:59 +1100

    Adjusting path names

--- forward/generate_tvb_data.py
+++ forward/generate_tvb_data.py
@@ -11,8 +11,8 @@ def main(region_id):
     """ TVB Simulation to generate raw source space dynamics, unit in mV, and ms
     :param region_id: int; source region id, with parameters generating interictal spike activity
     """
-    if not os.path.isdir('../source/raw_nmm/a{}/'.format(region_id)):
-        os.mkdir('../source/raw_nmm/a{}/'.format(region_id))
+    if not os.path.isdir('../DeepSIF/source/raw_nmm/a{}/'.format(region_id)):
+        os.mkdir('../DeepSIF/source/raw_nmm/a{}/'.format(region_id))
     start_time = time.time()
     print('------ Generate data of region_id {} ----------'.format(region_id))
     conn = connectivity.Connectivity.from_file(source_file=os.getcwd()+'/../anatomy/connectivity_76.zip') # connectivity provided by TVB
